2a1f6bd8afa7ae35e8ca3e7ce33e1de0bca8c783
Murat Tasan <mmuurr@gmail.com>
2018-02-24 13:05:14 -0700

    better tz handling to more closely match strftime behavior

--- R/iso8601.R
+++ R/iso8601.R
@@ -2,18 +2,26 @@
 #' @param x a Date or POSIXt time vector.
 #' @param what [string] What type of output?
 #' "datetime" and "timestamp" are equivalent, "date" is just the date, and "time" is just the time (i.e. without date).
-#' @param tz [Olson timezone string] The desired timezone for the output string.
+#' @param tz [Olson timezone string] The desired timezone for output string presentation; same as in `base:strftime()`.
+#'        The default (empty string) uses R's current session-set timezone.
+#'        (`NULL` is treated identically to the empty string.)
 #' @param tz_offset [boolean] Should the timezone offset be included in the output?
 #' @param digits [integer] Subsecond digits desired in the output string, as specified by the "%OSn" format string in `base::strftime()`, where n is the number of digits.
 #' @return An ISO8601-formatted character vector.
-iso8601 <- function(x, what = c("datetime", "timestamp", "date", "time"), tz = NULL, tz_offset = TRUE, digits = 0) {
+iso8601 <- function(x, what = c("datetime", "timestamp", "date", "time"), tz = "", tz_offset = TRUE, digits = 0) {
+    ## process `what`; datetime == timestamp
     what <- match.arg(what)
     if(what == "datetime") what <- "timestamp"
 
-    if(!is.null(tz) && is.na(match(tz, OlsonNames()))) {
-        stop(sprintf("unrecognized tz = %s", tz))
+    ## check for valid tz:
+    if(is.null(tz)) {
+        tz <- ""
+        warning("NULL tz detected, using system timezone as default", immediate. = TRUE)
     }
-    if(is.null(tz)) tz <- ""
+    stopifnot(is.character(tz) && length(tz) == 1)
+    if(!identical(tz, "") && is.na(match(tz, OlsonNames()))) {
+        stop(sprintf("unrecognized tz (%s)", tz))
+    }
     
     digits <- as.integer(digits)
 
